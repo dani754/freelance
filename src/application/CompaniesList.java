a95f3290f733b96478f12d84a21ec647fbf5d489
Danielle Vaitsman <d734051@gmail.com>
2021-07-29 13:55:31 +0300

    companies screen buttons

--- src/application/CompaniesList.java
+++ src/application/CompaniesList.java
@@ -1,12 +1,15 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
@@ -18,6 +21,12 @@ public class CompaniesList implements Initializable {
 	private ListView<String> companiesListView;
 	@FXML
 	private Label headline;
+	@FXML
+	private Button logout;
+	@FXML
+	private Button newCopmany;
+	@FXML 
+	private Button selectCompany;
 	
 	public CompaniesList() {
 		// TODO Auto-generated constructor stub
@@ -38,4 +47,17 @@ public class CompaniesList implements Initializable {
 			}
 		});
 	}
+	
+	public void selectCompany(ActionEvent e) throws IOException, Exception {
+		System.out.println("the selected company is: " + _currentCompany);
+	}
+	
+	public void newCompany(ActionEvent e) throws IOException, Exception {
+		System.out.println("new company screen");
+	}
+	
+	public void logOut(ActionEvent e) throws IOException, Exception {
+		System.out.println("return to logIn screen");
+	}
+
 }
